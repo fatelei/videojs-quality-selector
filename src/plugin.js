79ccf8cc206d0e87346a28ba59c41794c257f01c
fatelei <fatelei@gmail.com>
2018-08-29 16:59:39 +0800

    Support video quality switch.

--- src/plugin.js
+++ src/plugin.js
@@ -2,9 +2,65 @@ import videojs from 'video.js';
 import {version as VERSION} from '../package.json';
 
 const Plugin = videojs.getPlugin('plugin');
+const MenuButton = videojs.getComponent('MenuButton');
+const MenuItem = videojs.getComponent('MenuItem');
 
 // Default options for the plugin.
 const defaults = {};
+
+/**
+ * Video quality item.
+ */
+class VideoQualityItem extends MenuItem {
+  constructor(player, options) {
+    super(player, options);
+  }
+
+  handleClick() {
+    this.selected(true);
+    this.player().qualitySelector().changeVideoQuality(this.options_.identify);
+  }
+}
+
+class QualitySelectorMenu extends MenuButton {
+  constructor(player, options) {
+    super(player, options);
+    this.addClass('vjs-quality-selector-menu');
+  }
+
+  createItems() {
+    const player = this.player();
+    const qualityLevels = player.options_.qualityLevels || [];
+    let hasAuto = false;
+
+    for (const level of qualityLevels) {
+      if (level.label === 'auto') {
+        hasAuto = true;
+        break;
+      }
+    }
+
+    if (!hasAuto) {
+      qualityLevels.push({
+        label: 'auto',
+        identify: 'sd'
+      });
+    }
+
+    return qualityLevels.map(item => {
+      const menuItem = new VideoQualityItem(player, {
+        label: item.label,
+        identify: item.identify
+      });
+
+      if (item.label === 'auto') {
+        this.el().innerHTML = `<span class='vjs-quality-selector-btn'>${item.label}</span>`;
+        menuItem.addClass('vjs-selected');
+      }
+      return menuItem;
+    });
+  }
+}
 
 /**
  * An advanced Video.js plugin. For more information on the API
@@ -34,7 +90,34 @@ class QualitySelector extends Plugin {
 
     this.player.ready(() => {
       this.player.addClass('vjs-quality-selector');
+      this.player.getChild('controlBar').addChild(
+        'qualityMenuButton', {}, this.player.children().length - 1);
     });
+  }
+
+  /**
+   * Change video quality.
+   */
+  changeVideoQuality(identify) {
+    const player = this.player;
+    const currentPlayAt = player.currentTime();
+    let src;
+    const qualityAry = player.options_.qualityLevels || [];
+
+    if (qualityAry.length !== 0) {
+      for (const level of qualityAry) {
+        if (level.identify === identify) {
+          src = level.src;
+          break;
+        }
+      }
+      if (player.currentSrc() !== src) {
+        player.src('//vjs.zencdn.net/v/oceans.mp4');
+        player.load();
+        player.currentTime(currentPlayAt);
+        player.play();
+      }
+    }
   }
 }
 
@@ -43,6 +126,9 @@ QualitySelector.defaultState = {};
 
 // Include the version number.
 QualitySelector.VERSION = VERSION;
+
+// Register menu button.
+videojs.registerComponent('qualityMenuButton', QualitySelectorMenu);
 
 // Register the plugin with video.js.
 videojs.registerPlugin('qualitySelector', QualitySelector);
